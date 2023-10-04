039ad6aa0080036097682012ca9d0dd8c46aeb90
vaishuan <Vaishnavi.Adilla@amdocs.com>
2023-10-04 14:48:36 +0530

    commit java files

---
+++ target/classes/vaishu.java
@@ -0,0 +1,15 @@
+package amdocs1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//public class vaishu {
+//		public static void main(String[] args) {
+//			FirefoxDriver dr=new FirefoxDriver();
+//			dr.get("http://www.google.com");
+//			dr.manage().window().maximize();
+//			dr.findElement(By.id("APjFqb")).sendKeys("mobile");
+//			dr.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).click();
+//			dr.findElement(By.xpath("/html/body/div[5]/div/div[12]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div/span/a/h3")).click();
+//			}
+//
+//	}
+
